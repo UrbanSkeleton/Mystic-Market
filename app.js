dfdf6d26f1c00c92741d6d7e83a46626305742d9
UrbanSkeleton <97252619+UrbanSkeleton@users.noreply.github.com>
2025-02-08 12:59:24 -0500

    redid express server

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
-import express from "express";
-import http from "http";
-import path from "path";
-import {Server as SocketIOServer} from "socket.io";
-import {fileURLToPath} from "url";
+const express = require("express");
+const app = express();
+const serv = require("http").Server(app);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.get("/",
+        function(req, res) { res.sendFile(__dirname + "/client/index.html"); });
+app.use("/client", express.static(__dirname + "/client"));
 
-class GameServer {
-  constructor() {
-    this.port = process.env.PORT || 3000;
+serv.listen(2000);
 
-    this.initServer();
-  }
-
-  initServer() {
-    this.app = express();
-    this.server = http.createServer(this.app);
-    this.io = new SocketIOServer(this.server);
-
-    this.setupRoutes();
-    this.setupSocketEvents();
-
-    this.server.listen(
-        this.port,
-        () => { console.log(`Server running on port ${this.port}`); });
-  }
-
-  setupRoutes() {
-    this.app.use("/shared", express.static(path.join(__dirname, "shared")));
-    this.app.use("/client", express.static(path.join(__dirname, "client")));
-
-    this.app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "index.html"));
-    });
-  }
-
-  setupSocketEvents() {}
-}
-
-new GameServer();
+console.log("Server Running");
