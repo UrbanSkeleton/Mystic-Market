9e52550778aa92e29ed80c726645fd47e8596d4a
UrbanSkeleton <97252619+UrbanSkeleton@users.noreply.github.com>
2025-02-08 13:58:00 -0500

    Basic Asset Stuff

--- app.js
+++ app.js
@@ -11,7 +11,41 @@ serv.listen(2000);
 console.log("Server Running");
 
 const io = require("socket.io")(serv, {});
+
+const socketList = {};
 io.sockets.on("connection", function(socket) {
   console.log("Client Connected");
-  socket.emit("helloWorld");
+
+  // sends initial asset data to the client appon connecting:
+  socket.emit("assetData", assetList.data);
+
+  const id = Math.random();
+  socketList[id] = socket;
 });
+
+class Asset {
+  constructor(name, initVal) {
+    this.name = name;
+    this.value = initVal;
+  }
+}
+
+class AssetList {
+  constructor() { this.assets = {}; }
+
+  addAsset(name, val) { this.assets[name] = new Asset(name, val); }
+
+  get data() {
+    const data = {};
+    for (let name in this.assets) {
+      data[name] = this.assets[name].value;
+    }
+    return data;
+  }
+}
+
+const assetList = new AssetList();
+assetList.addAsset("potion", 100);
+assetList.addAsset("sword", 100);
+assetList.addAsset("mushroom", 100);
+assetList.addAsset("ironOre", 100);
